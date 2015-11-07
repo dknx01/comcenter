223d1c0cb2d4d0f31cbea1f5a837c18770d741e6
dknx01 <e.witthauer@gmail.com>
2015-11-07 11:50:39 +0100

    ApiTest

--- src/AppBundle/Tests/Service/Twittter/ApiTest.php
+++ src/AppBundle/Tests/Service/Twittter/ApiTest.php
@@ -2,8 +2,44 @@
 
 namespace AppBundle\Tests\Service\Twittter;
 
+use AppBundle\Service\Twitter\Api;
+use PHPUnit_Framework_MockObject_MockObject;
+use PHPUnit_Framework_TestCase;
+use TwitterAPIExchange;
 
-class ApiTest extends \PHPUnit_Framework_TestCase
+class ApiTest extends PHPUnit_Framework_TestCase
 {
+
+    /**
+     * @var TwitterAPIExchange|PHPUnit_Framework_MockObject_MockObject
+     */
+    private $api;
+
+    public function setUp()
+    {
+        $this->api = $this->getMockBuilder('TwitterAPIExchange')
+            ->disableOriginalConstructor()
+            ->getMock();
+    }
+
+    public function testGetTimeline()
+    {
+        $this->api->expects($this->once())
+            ->method('setGetfield')
+            ->with('?count=20')
+            ->willReturnSelf();
+        $this->api->expects($this->once())
+            ->method('buildOauth')
+            ->with('https://api.twitter.com/1.1/statuses/home_timeline.json', Api::GET)
+            ->willReturnSelf();
+        $responseJson ='[{twitter:"api"}]';
+        $this->api->expects($this->once())
+            ->method('performRequest')
+            ->willReturn($responseJson);
+
+        $api = new Api($this->api);
+        $this->assertEquals($responseJson, $api->getTimeline());
+    }
+
 
 }
